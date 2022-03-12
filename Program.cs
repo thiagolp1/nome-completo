645d1f4dd235cce0faae63aeecaa75c9ecdbfca4
thiago <thiagolopespereiralp1@gmail.com>
2022-03-11 22:41:41 -0300

    termino da liçao

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿String nome, sobrenome, sobrenomeM;
+Console.Write("Digite seu primeiro nome: ");
+nome=Console.ReadLine()!;
+
+Console.Write("Digite seu segundo nome: ");
+sobrenome=Console.ReadLine()!;
+
+sobrenomeM=sobrenome.ToUpper();
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+
+Console.WriteLine($"Nome de catálogo: {sobrenomeM}, {nome}");
